4ed56db9033fea3bb51d13222b0cd1f73eab86f8
Potwings <ygk0719@gmail.com>
2025-03-14 23:59:31 +0900

    대역대 추가 메소드, 아이피 포함 여부 확인 메소드 테스트 2개 통과

--- src/main/java/org/potwings/ipchecker/service/IPRangeCheckService.java
+++ src/main/java/org/potwings/ipchecker/service/IPRangeCheckService.java
@@ -1,43 +1,76 @@
 package org.potwings.ipchecker.service;
 
-import java.util.HashMap;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Map;
+import java.util.TreeMap;
+import lombok.extern.log4j.Log4j2;
+import org.apache.commons.net.util.SubnetUtils;
+import org.apache.commons.net.util.SubnetUtils.SubnetInfo;
 import org.springframework.stereotype.Service;
 
+@Log4j2
 @Service
 public class IPRangeCheckService {
 
   // key: 범위 start, value: 범위 end 형식으로 아이피 범위를 저장
-  private Map<Long, Long> ipRangeMap = new HashMap<>();
+  private TreeMap<Long, Long> ipRangeMap = new TreeMap<>();
 
   /**
    * 아이피 입력 시 Map에 있는 Range들을 확인하여 포함되는지 여부를 반환하는 메소드
+   *
    * @param ip
    * @return
    */
   public boolean isIncludeIP(String ip) {
 
-    return false;
+    Long ipLong = ipToLong(ip);
+    if (ipLong == null) {
+      return false;
+    }
+
+    Map.Entry<Long, Long> entry = ipRangeMap.floorEntry(ipLong);
+    return entry != null && ipLong >= entry.getKey() && ipLong <= entry.getValue();
   }
 
   /**
-   * SubnetUtils를 통하여 대역대의 lowIp와 high 아이피를 불러옴
-   * 그 후 해당 아이피들을 long으로 변환하여 key: start, value: end로 Map에 저장
+   * SubnetUtils를 통하여 대역대의 lowIp와 high 아이피를 불러옴 그 후 해당 아이피들을 long으로 변환하여 key: start, value: end로
+   * Map에 저장
+   *
    * @param ipRange
    */
-  public void addRange(String ipRange) {
+  public boolean addRange(String ipRange) {
+    SubnetUtils subnetUtils = new SubnetUtils(ipRange);
+    SubnetInfo info = subnetUtils.getInfo();
+    Long rangeStart = ipToLong(info.getLowAddress());
+    Long rangeEnd = ipToLong(info.getHighAddress());
+    if (rangeStart == null || rangeEnd == null) {
+      return false;
+    }
 
+    ipRangeMap.put(rangeStart, rangeEnd);
+
+    return true;
   }
 
   /**
-   * IP를 숫자(Long)으로 변환해주는 메소드
-   * 클래스 내부에서만 쓰이므로 private로 선언
-   * 테스트는 어떻게 진행할까???
+   * IP를 숫자(Long)으로 변환해주는 메소드 클래스 내부에서만 쓰이므로 private로 선언 테스트는 어떻게 진행할까??? -> 모든 메소드의 테스트 코드를 만들 필요는
+   * 없다. private는 public 메소드를 테스트하면서 간접적으로 테스트 될 것
+   *
    * @param ip
    * @return
    */
-  private long ipToLong(String ip) {
-
-    return 0L;
+  private Long ipToLong(String ip) {
+    byte[] bytes = null;
+    try {
+      bytes = InetAddress.getByName(ip).getAddress(); // IP를 바이트 배열로 변환
+    } catch (UnknownHostException e) {
+      log.warn("IP Parse Fail : ip: {}", e.getMessage(), e);
+    }
+    long result = 0;
+    for (byte b : bytes) {
+      result = (result << 8) | (b & 0xFF); // 왼쪽 시프트 후 OR 연산
+    }
+    return result;
   }
 }
--- src/test/java/org/potwings/ipchecker/service/IPRangeCheckServiceTests.java
+++ src/test/java/org/potwings/ipchecker/service/IPRangeCheckServiceTests.java
@@ -1,8 +1,10 @@
 package org.potwings.ipchecker.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.apache.commons.net.util.SubnetUtils;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,55 +15,75 @@ public class IPRangeCheckServiceTests {
 
   @Autowired private IPRangeCheckService ipRangeCheckService;
 
+  /**
+   * 대역대 범위에 대해 확인하려하는데 어떻게 테스트를 진행해야할까?
+   * 범위를 추가한 후에 해당 범위가 정상적으로 ipRangeMap에 포함되었는지 확인할 것
+   * 허나 ipRangeMap는 service 클래스 내부에서만 범위 확인을 위해서만 쓰이는데
+   * 테스트를 위해 외부에 노출시키는 것이 맞을까??
+   *
+   * 만일 다른 방법으로 정상적으로 추가되었는지 확인한다면 어떻게 확인해야할까??
+   * 대역대를 addRange메소드를 통하여 추가한 후 isIncludeIP 메소드를 통하여 확인 진행하자
+   *
+   * 현재 addRangeTest에서는 정상적으로 실행되는지만 확인
+   * 추후 isIncludeIP 테스트에서 정확한 값이 추가되었는지 간접적으로 확인 가능
+   */
   @Test
   @DisplayName("대역대 추가 테스트")
   public void addRangeTest() {
 
-    /*
-     * 대역대 범위에 대해 확인하려하는데 어떻게 테스트를 진행해야할까?
-     * 범위를 추가한 후에 해당 범위가 정상적으로 ipRangeMap에 포함되었는지 확인할 것
-     * 허나 ipRangeMap는 service 클래스 내부에서만 범위 확인을 위해서만 쓰이는데
-     * 테스트를 위해 외부에 노출시키는 것이 맞을까??
-     *
-     * 만일 다른 방법으로 정상적으로 추가되었는지 확인한다면 어떻게 확인해야할까??
-     * 대역대를 addRange메소드를 통하여 추가한 후 isIncludeIP 메소드를 통하여 확인 진행하자
-     *
-     *
-     * 근데 하나의 테스트 코드에서는 하나의 메소드만 테스트 해야하지 않을까??
-     */
-
     // arrange
     String ipRange = "192.168.1.0/24";
+
+    // act
+    boolean result = ipRangeCheckService.addRange(ipRange);
+
+    // assert
+    assertThat(result).isTrue();
+  }
+
+  /**
+   * 대역대에 해당하는 아이피가 추가되어 있을 경우 정상적으로 테스트 되는지 확인
+   */
+  @Test
+  @DisplayName("아이피 포함 여부 확인 테스트")
+  public void isIncludeIPTest() {
+    // arrange
+    String ipRange = "192.168.1.0/24";
+    ipRangeCheckService.addRange(ipRange);
     String ip = "192.168.247";
 
     // act
-    ipRangeCheckService.addRange(ipRange);
+    boolean result = ipRangeCheckService.isIncludeIP(ip);
 
     // assert
-    //    boolean isInclude = ipRangeCheckService.isIncludeIP(ip); // 포함 여부
-    //    assertThat(isInclude).isTrue();
+    assertThat(result).isTrue();
   }
 
+   /**
+   * 중복되는 범위를 가지고 있는 대역대가 이미 등록되어 있는 경우를 위한 테스트
+   * 만일 단순 숫자만으로 이야기 한다면
+   * 10~100이 이미 등록되어 있을 때 1~11이 추가된다면
+   * 1~100으로 변환하여 등록되어야 한다.
+   *
+   * 1. 등록될 때 마다 Map을 새로 생성
+   * 2. 중복되는 범위를 확인 후 중복되는 범위들만 수정하여 entry 추가
+   */
   @Test
   @DisplayName("중복 대역대 추가 테스트")
   public void addDupeRangeTest() {
-    /*
-     * 중복되는 범위를 가지고 있는 대역대가 이미 등록되어 있는 경우를 위한 테스트
-     * 만일 단순 숫자만으로 고려한다면
-     * 10~100이 이미 등록되어 있을 때 1~11이 추가된다면
-     * 1~100으로 변환하여 등록되어야 한다.
-     *
-     * 1. 등록될 때 마다 Map을 새로 생성
-     * 2. 중복되는 범위를 확인 후 중복되는 범위들만 수정하여 entry 추가
-     */
   }
 
+  /**
+   * IPv6의 경우 Long 값으로 변환이 불가능하므로 다른 방법을 고안해야한다.
+   */
   @Test
-  @DisplayName("중복 대역대 추가 테스트")
-  public void isIncludeIPTest() {
-    /*
-     * 대역대에 해당하는 아이피가 추가되었을 경우 정상적으로 테스트 되는지 확인
-     *
-     */
+  @DisplayName("IPv6 테스트")
+  public void isIncludeIPv6Test() {
+    // arrange
+    String ip = "fe80::db6c:a3ec:6c94:5f70%10";
+
+    // act
+
+    // assert
   }
 }
